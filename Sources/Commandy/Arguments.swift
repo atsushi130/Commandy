fa470a1088a4b58cba85196bbd812f7e1c679c60
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 15:45:07 +0900

    implement: flyweight Arguments

---
+++ Sources/Commandy/Arguments.swift
@@ -0,0 +1,14 @@
+//
+//  Arguments.swift
+//  Commandy
+//
+//  Created by Atsushi Miyake on 2018/07/28.
+//
+
+import Foundation
+
+public final class Arguments {
+    
+    public static let shared = Arguments()
+    private let arguments: [String] = ProcessInfo.processInfo.arguments.dropFirst().map { $0 }
+}
