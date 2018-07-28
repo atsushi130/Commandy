2f99554e3e7be374075209c19666e9da1e56f8c5
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 15:43:27 +0900

    remove: Commandy swift file

--- Sources/Commandy/Commandy.swift
+++
@@ -1,3 +0,0 @@
-struct Commandy {
-    var text = "Hello, World!"
-}
--- Tests/CommandyTests/CommandyTests.swift
+++ Tests/CommandyTests/CommandyTests.swift
@@ -6,7 +6,7 @@ class CommandyTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Commandy().text, "Hello, World!")
+        XCTAssertEqual(true, true)
     }
 
 
