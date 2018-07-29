b8dc0c6e8e417ad25f40e69f15486d38506db2b3
atsushi130 <atsushi_main@outlook.jp>
2018-07-29 16:23:11 +0900

    implement: options (temp)

--- Sources/Commandy/Command.swift
+++ Sources/Commandy/Command.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 
-public protocol Command {
-    static func run() throws
+public protocol Command: CaseIterable {
+    func run() throws
 }
+
+public extension Command where Self: RawRepresentable, Self.RawValue == String {
+    
+    public var option: String {
+        let option = self.rawValue.replacingOccurrences(of: "([A-Z])", with: "-$1", options: .regularExpression).lowercased()
+        let splitedOption = option.split(separator: "-")
+        
+        if splitedOption.count >= 2 {
+            return "--" + option
+        } else {
+            return "-" + option
+        }
+    }
+    
+    static var command: Self? {
+        return self.options.first
+    }
+    
+    static var options: [Self] {
+        let options = Arguments.shared.options
+        return Self.allCases.filter { `case` in
+            return options.contains(`case`.option)
+        }
+    }
+}
+
+
