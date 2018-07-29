e441dfd813b2bdf3157d4f60ddbcf30130ae77f7
atsushi130 <atsushi_main@outlook.jp>
2018-07-29 15:18:01 +0900

    add: CaseIterable

---
+++ Sources/Commandy/CaseIterable.swift
@@ -0,0 +1,33 @@
+//
+//  CaseIterable.swift
+//  Commandy
+//
+//  Created by Atsushi Miyake on 2018/07/29.
+//
+
+public protocol CaseIterable {
+    static var allCases: [Self] { get }
+}
+
+extension CaseIterable where Self: Hashable {
+    
+    private static var iterator: AnyIterator<Self> {
+        var n = 0
+        return AnyIterator {
+            defer { n += 1 }
+            let next = withUnsafePointer(to: &n) {
+                UnsafeRawPointer($0).assumingMemoryBound(to: Self.self).pointee
+            }
+            return next.hashValue == n ? next : nil
+        }
+    }
+    
+    public static var allCases: [Self] {
+        return Array(self.iterator)
+    }
+}
+
+enum Test: CaseIterable {
+    case test1
+    case test2
+}
